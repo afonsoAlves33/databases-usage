535097b204f6e6ba267d717603b6c6312bd2463a
afonsoAlves33 <afonso.alves161110@gmail.com>
2024-08-21 10:42:30 -0300

    make the main route recovery the database and make a route for adding a person to the Pessoa table

--- postgres/app/main.py
+++ postgres/app/main.py
@@ -7,10 +7,11 @@ DB_URL = "postgres://admin:admin@db:5432/main"
 app = FastAPI()
 
 
+
 @app.get("/")
 def read_root():
     try:
-        with psycopg.connect("dbname=main user=admin password=admin") as conn:
+        with psycopg.connect("postgres://admin:admin@postgres:5432/main") as conn:
             print(conn)
             with conn.cursor() as cur:
                 cur.execute("""
@@ -21,16 +22,26 @@ def read_root():
                 idade INT CHECK (idade >= 0)
             );
             """)
-                conn.commit() 
-                conn.close()
-
+                conn.commit()
+                conn.close() 
     except Exception as e:
         print(e)
         return "Exceção: ", e
-
-    
     return {"Table Pessoa criada"}
 
-@app.post("/asa")
-def add_person(name: str, email: str, age: int):
-    pass
+@app.post("/insert_person")
+async def add_person(name: str, email: str, age: int):
+    try:
+        with psycopg.connect("postgres://admin:admin@postgres:5432/main") as conn:
+            print(conn)
+            with conn.cursor() as cur:
+                cur.execute("""
+                    INSERT INTO pessoa(nome, email, idade)
+                    VALUES (%s, %s, %s);
+                """, (name, email, age))
+                conn.commit()
+                conn.close() 
+
+    except Exception as e:
+        return {"Exceção": f"{e}"}
+    return {"Sucesso"}
